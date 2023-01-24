ee3581216b4dca7710468c19c2afcf0f220e81cc
Marc MASSE <marc.masse600@orange.fr>
2023-01-24 23:03:04 +0100

    UserDTO refactored to add validated condition

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/DTO/UserDTO.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/DTO/UserDTO.java
@@ -1,6 +1,7 @@
 package com.nnk.springboot.DTO;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
@@ -22,6 +23,9 @@ public class UserDTO {
     
     @NotBlank(message = "Password is mandatory")
     @Size(max = 125, message = "125 characters maximum are allowed")
+    @Size(min = 8, message = "8 characters minimum are allowed")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,125}$", 
+    	message = "Password must contain at least 1 uppercase, 1 special character and 1 digit.")
     private String password;
     
     @NotBlank(message = "FullName is mandatory")
